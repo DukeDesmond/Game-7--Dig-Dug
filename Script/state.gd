30eb78b2904e6560aa4ae20541fbd0b44ceea603
DukeDesmond <ukamran1@outlook.com>
2024-09-24 10:10:20 +0500

    Initial Commit
    Just setting stuff up, no major progress so far.

---
+++ Script/state.gd
@@ -0,0 +1,20 @@
+class_name State extends Node
+
+signal transitioned
+var player : CharacterBody2D
+
+
+func enter():
+	pass
+
+func exit():
+	pass
+	
+func state_process(_delta : float):
+	pass
+
+func state_physics_process(_delta : float):
+	pass
+
+func state_input(event : InputEvent):
+	pass
